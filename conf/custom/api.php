87b5f0c7ed6bc90c4bafdbef3b43d7d16825d319
rmontano <rmontano.jegra@gmail.com>
2021-05-12 09:16:07 -0500

    remove keys

--- conf/custom/api.php
+++ conf/custom/api.php
@@ -1,7 +1,14 @@
 <?php
 
+<<<<<<< HEAD
 // Inbenta API configuration
 return [
     'key' => '',
     'secret' => ''
 ];
+=======
+return array(
+        'key'           => '',
+        'secret'        => ''
+);
+>>>>>>> dc6ddc8... remove keys
